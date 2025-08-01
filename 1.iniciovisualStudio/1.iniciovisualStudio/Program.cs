0b94a572eebdf907f0ea901c2bc49be1b6e29d24
Juan Luis <juanl.echeverrir@upb.edu.co>
2025-08-01 09:33:34 -0500

    Variables y constantes

--- 1.iniciovisualStudio/1.iniciovisualStudio/Program.cs
+++ 1.iniciovisualStudio/1.iniciovisualStudio/Program.cs
@@ -10,6 +10,15 @@ namespace _1.iniciovisualStudio
     {
         static void Main(string[] args)
         {
+            //Declaracion e inicializacion de variables
+            string nombre="Juan";
+            const string iva = "19%";
+            string apellidos = "Echeverri Ruiz";
+            Console.Write(nombre+" Bienvenido al curso de Fundamentos de Programacion\n");
+            nombre = "Luis";
+            //iva = "20%"; No puedo cambiar el valor de iva por ser una constante
+            Console.Write("\t" + nombre +" "+ apellidos +" "+ iva);
+            Console.ReadKey();
         }
     }
 }
