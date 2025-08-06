230403c095b3a9d568c2eee1bfc313ad895fe724
JuanL0914 <Juanl.echeverrir@upb.edu.co>
2025-08-06 09:34:12 -0500

    Add files via upload

---
+++ 2.operadores/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operadores
+            //Operadores Numericos
+            //Cambio de signo
+            int num1 = 5;
+            int num2 = -num1;
+            int num3 = +num2;
+            int num4 = -num3;
+
+            Console.WriteLine(num1 + "//" + num2 + "//" + num3 + "//" + num4);
+
+
+            //Aritmeticos
+            int num5 = 3 + 5; //suma
+            Console.WriteLine(num5);
+            int num6 = 100-26; //resta
+            Console.WriteLine(num6);
+            int num7 = 4 * 2;//Producto
+            Console.WriteLine(num7);
+            float num8 = 5f / 3f;//Division
+            Console.WriteLine(num8);
+            double num9 = 105d / 4;
+            Console.WriteLine(num9);
+            decimal num10 = 50m / 6;
+            Console.WriteLine(num10);
+            //incremento / decremento
+            num10++;//num10 = num10 + 1; Incremento
+            Console.WriteLine(num10);
+            num1--;//num1=num1 - 1; Decremento
+            Console.WriteLine(num1);
+            num2 += 5;//num2 = num2 + 5;
+            Console.WriteLine(num2);
+            num3 -= 5;//num3 = num3 - 5;
+            Console.WriteLine(num3);
+            num4 *= 5;//num4 = num4 * 5;
+            Console.WriteLine(num4);
+            num5 /= num1;//num5 = num5 / num1;
+            Console.WriteLine(num5);
+
+            //Orden de evaluacion operadores aritmeticos
+            float num11 = 4f * 3 / 2;
+            Console.WriteLine(num11);
+            float num12 = 4 / 3 * 2f;
+            Console.WriteLine(num12);
+            float num13 = (4 * 3) / 2;
+            Console.WriteLine(num13);
+            float num14 = 4 + 6 * 2;
+            Console.WriteLine(num14);
+            float num15 = 4 + 6 * (2 - 1);
+            Console.WriteLine(num15);
+
+            //Operadores logicos
+            //Conjuncion - And -Y -&&
+            Console.WriteLine("TABLA DE VERDAD CONJUNCION");
+            Console.WriteLine($"V && V ={true && true}");
+            Console.WriteLine($"V && F ={true && false}");
+            Console.WriteLine($"F && V ={false && true}");
+            Console.WriteLine($"F && F ={false && false}");
+            //Disyucion - OR - ||
+            Console.WriteLine("TABLA DE VERDAD DISYUCION");
+            Console.WriteLine($"V || V ={true || true}");
+            Console.WriteLine($"V || F ={true || false}");
+            Console.WriteLine($"F || V ={false || true}");
+            Console.WriteLine($"F || F ={false || false}");
+
+            bool exp1 = true;
+            bool exp2 = false;
+            bool exp3 = exp1 && exp2;
+            bool exp4 = !exp2;
+            bool exp5 = !exp3 || exp4;
+            Console.WriteLine($"exp1={exp1}, exp2={exp2}, exp3={exp3}, exp4={exp4}, exp5={exp5}");
+            Console.ReadKey();
+        }
+    }
+}
