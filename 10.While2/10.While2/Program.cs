b5b2052f301714e125beb3f8d75d86224a5e10d8
JuanL0914 <Juanl.echeverrir@upb.edu.co>
2025-08-29 09:32:58 -0500

    ClicloWhile

---
+++ 10.While2/10.While2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _10.While2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //crear un algoritmo que permita calcular la factorial de un numero dado por el usuario. (5! = 1*2*3*4*5)
+            int contador = 1;
+            int acumulador = 1;
+            int factorial = 0;
+
+        }
+    }
+}
+
