c5d1b3d85506d94489f6998675b48aad4228de56
JuanL0914 <Juanl.echeverrir@upb.edu.co>
2025-09-12 09:31:39 -0500

    Taller 2 Ciclos

---
+++ 21.Taller Parcial Ciclos/21.Taller Parcial Ciclos/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _21.Taller_Parcial_Ciclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            //1.
+        {
+            float calificacion = 0;
+            int contador = 0;
+            string respuesta = " ";
+            float Resultados = 0;
+            do
+            {
+                Console.WriteLine("Ingrese la calificacion");
+                calificacion = int.Parse(Console.ReadLine());
+
+                
+                Console.WriteLine("Desea ingresar otra nota? s:si n:no");
+                respuesta = Console.ReadLine().ToLower();
+
+                Resultados += calificacion;
+
+                 contador++;
+            } while (respuesta == "s");
+  
+            Console.WriteLine($"El promedio de calificaciones es, {Resultados/contador}");
+        }
+    }
+}
