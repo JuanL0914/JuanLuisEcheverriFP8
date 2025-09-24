99afd6450bdc51b035eb51b6820f9eb2ba25ff9d
JuanL0914 <Juanl.echeverrir@upb.edu.co>
2025-09-24 09:29:59 -0500

    Taller vectores

---
+++ 25.Taller Vectores.arreglos/25.Taller Vectores.arreglos/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace _25.Taller_Vectores.arreglos
+    //1.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  int[] vector; //declarando
+                vector = new int[15];//inicializando
+                int mayor = 0;
+                int menor = 0;
+
+                for (int i = 0; i < vector.Length; i++)
+                {
+                    Console.WriteLine($"Ingrese el numero entero ´para la posicion {i+1}, con indice {i}:");
+                    vector[i]=int.Parse(Console.ReadLine()); 
+
+                    if (i == 0)
+                    {
+                        mayor = vector[i];
+                        menor = vector[i];
+                    }
+                    else
+                    {
+                        if (vector[i] > mayor)
+                        {
+                        mayor = vector[i];
+                        }
+                        if (vector[i] < menor)
+                        {
+                        menor = vector[i];
+                        }
+                    }
+                        Console.Clear();
+                }
+                for (int i = 0; i < vector.Length; i++)
+                {
+                    Console.Write(vector[i] + " |");
+                }
+
+                Console.WriteLine($"\nEl numero mayor es {mayor}, el numero menor es {menor}");*/
+
+            //2.
+            int[] vector; 
+            vector = new int[15];
+            int mayor = 0;
+            int menor = 0;
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine($"Ingrese el numero entero ´para la posicion {i + 1}, con indice {i}:");
+                vector[i] = int.Parse(Console.ReadLine());
+
+                if (i == 0)
+                {
+                    mayor = vector[i];
+                    menor = vector[i];
+                }
+                else
+                {
+                    if (vector[i] > mayor)
+                    {
+                        mayor = vector[i];
+                    }
+                    if (vector[i] < menor)
+                    {
+                        menor = vector[i];
+                    }
+                }
+                Console.Clear();
+            }
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.Write(vector[i] + " |");
+            }
+
+            Console.WriteLine($"\nEl numero mayor es {mayor}, el numero menor es {menor}");
+        }
+    }
+}
+
+    
