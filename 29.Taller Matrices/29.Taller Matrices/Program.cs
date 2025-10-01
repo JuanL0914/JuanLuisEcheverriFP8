5f32fad7185f57e1545b173814cb7a289be02f30
JuanL0914 <Juanl.echeverrir@upb.edu.co>
2025-10-01 09:32:11 -0500

    Taller Matrices

---
+++ 29.Taller Matrices/29.Taller Matrices/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _29.Taller_Matrices
+{
+    /*1. Desarrollar un programa que crea una matriz de 10 filas y 20 columnas y muestre por
+pantalla la suma de los elementos de cada columna.
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          /*  int[,] numeros = new int[4, 4];
+            int[] sumaColumnas = new int[numeros.GetLength(1)];
+            //recorrer la matrix para llenarla
+            for (int i = 0; i < numeros.GetLength(0); i++)
+            {
+                for (int j = 0; j < numeros.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Ingrese el valor para la posicion {i},{j}");
+                    
+                    numeros[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            //recorrer la matrix para mostrarla en pantalla
+            for (int i = 0; i < numeros.GetLength(0); i++)
+            {
+                for (int j = 0; j < numeros.GetLength(1); j++)
+                {
+                    Console.Write($"{numeros[i,j]} |");
+                }
+                Console.WriteLine( );
+            //recorrer la matrix para mostrarla en sumar los elementos de cada columna
+            }
+            for (int i = 0; i < numeros.GetLength(1); i++)
+            {
+                for (int j = 0; j < numeros.GetLength(0); j++)
+                {
+                    sumaColumnas[i] += numeros[j,i];
+                }
+            }
+            //Recorrer el vector y mostrar la suma de cada columna
+            for (int i = 0;i < numeros.GetLength(0); i++)
+            {
+                Console.Write($"{sumaColumnas[i]} |");
+            }*/
+
+
+
+/*2.Desarrollar un programa que crea una matriz de n filas* m columnas, el usuario ingresa
+caracteres en cada posición de la matriz hasta llenarla. El programa debe intercambiar la
+primera fila con la última fila de la matriz. Al final se debe imprimir la matriz original, y la
+matriz con el intercambio de filas*/
+           
+
+        }
+    }
+}
