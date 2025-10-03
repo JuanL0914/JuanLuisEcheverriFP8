c331652dca638d452a539b28211ac9b64bd6ef21
JuanL0914 <Juanl.echeverrir@upb.edu.co>
2025-10-03 09:35:35 -0500

    Programacion Modular

---
+++ 30. ProgramacionModular/30. ProgramacionModular/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _30._ProgramacionModular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje();
+            MostrarMensajes("Mi nombre es lucho");
+            //MostrarMensajes("Tengo 19 años");
+            //Console.WriteLine($"Edad calculada: {CalcularEdad()}");
+            MostrarMensajes($"Tengo {CalcularEdad(2025, 2006)}años");
+        }
+
+        //modulo 1 - Procedimiento
+        static void MostrarMensaje()
+        {
+            Console.WriteLine("Bienvenido a la clase de Programacion Modular");
+            MostrarMensajes("(Estoy desde Procedimiento sin parametros)");
+        }
+        static void MostrarMensajes(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+
+        //Modulo 3 - Funciones
+        static int CalcularEdad()
+        {
+            int edad;
+            edad = 2025 - 2006;
+            return edad;
+        }
+        
+        //Modulo 4
+        static int CalcularEdad(int añoActual, int añoNacimiento)
+        {
+            return añoActual - añoNacimiento;
+        }
+    }
+}
---
+++ 31.Modular 1/31.Modular 1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _31.Modular_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] multiplosDe5 = new int[CapturarFilas(), CapturarColumnas()];
+        }
+        static int[,] LlenarMatriz(int[,] matriz)
+        {
+            return matriz;
+        }
+
+        static int CapturarFilas()
+        {
+            Console.WriteLine("Ingrese el numero de filas para la matriz:");
+            int filas = Convert.ToInt32(Console.ReadLine());
+            return filas;
+        }
+        static int CapturarColumnas()
+        {
+            Console.WriteLine("Ingrese el numero de columnas para la matriz:");
+            int columnas = Convert.ToInt32(Console.ReadLine());
+            return columnas;
+        }
+    }  
+}
