cae312bf7756b2bea84449d5a1b123dfa778fb63
JuanL0914 <Juanl.echeverrir@upb.edu.co>
2025-10-15 08:55:28 -0500

    Add files via upload

---
+++ 25.Arreglos/25.Arreglos/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _25.Arreglos
+    //25. Algoritmo que permita solicitar 10 números, los cuales serán almacenados en un arreglo unidimensional, al final, debe visualizar el promedio de esos números.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
